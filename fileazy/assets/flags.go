2565b95163c02c1975e4c4338cd86d3970b7ed83
TRC Loop <aro.yt.mail@gmail.com>
2024-11-16 00:28:28 +0100

    Add a flag to just search for a file.

--- fileazy/assets/flags.go
+++ fileazy/assets/flags.go
@@ -1,7 +1,8 @@
 package assets
 
 const (
-	FileHelp       = "Only show Opening Menu and Open File with Program"
-	NoOpenHelp     = "Only show Fuzzy Search Menu without Opening the File"
-	OpenConfigHelp = "Open Config(Settings) Menu"
+	FileHelp          = "Only show Opening Menu and Open File with Program"
+	NoOpenHelp        = "Only show Fuzzy Search Menu without Opening the File"
+	OpenConfigHelp    = "Open Config(Settings) Menu"
+	SearchForFileHelp = "Search for a File, Will show a list of Files/Paths."
 )
--- fileazy/main.go
+++ fileazy/main.go
@@ -11,12 +11,13 @@ func main() {
 	file := flag.String("f", "", assets.FileHelp)
 	noOpen := flag.Bool("no", false, assets.NoOpenHelp)
 	openConfig := flag.Bool("c", false, assets.OpenConfigHelp)
+	searchForFile := flag.String("s", "", assets.SearchForFileHelp)
 
 	// Parse Flags
 	flag.Parse()
 
 	// Print Flags (for Debugging)
-	fmt.Printf("Flags: file: %s, noOpen: %t, openConfig: %t\n", *file, *noOpen, *openConfig)
+	fmt.Printf("Flags: file: %s, noOpen: %t, openConfig: %t searchForFile: %s\n", *file, *noOpen, *openConfig, *searchForFile)
 
 	// TODO: Implement Configmenu, Fuzzysearch(Menu), Openmenu.
 }

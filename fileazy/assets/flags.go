6e412ea380300b0a3585a1459cd5dbf8906b45e2
TRC Loop <aro.yt.mail@gmail.com>
2024-11-16 00:17:40 +0100

    Implemented three Flags.

---
+++ fileazy/assets/flags.go
@@ -0,0 +1,7 @@
+package assets
+
+const (
+	FileHelp       = "Only show Opening Menu and Open File with Program"
+	NoOpenHelp     = "Only show Fuzzy Search Menu without Opening the File"
+	OpenConfigHelp = "Open Config(Settings) Menu"
+)
--- fileazy/main.go
+++ fileazy/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fileazy/assets"
+	"flag"
+	"fmt"
+)
+
 func main() {
-	// TODO: implement
+	// Flags
+	file := flag.String("f", "", assets.FileHelp)
+	noOpen := flag.Bool("no", false, assets.NoOpenHelp)
+	openConfig := flag.Bool("c", false, assets.OpenConfigHelp)
+
+	// Parse Flags
+	flag.Parse()
+
+	// Print Flags (for Debugging)
+	fmt.Printf("Flags: file: %s, noOpen: %t, openConfig: %t\n", *file, *noOpen, *openConfig)
+
+	// TODO: Implement Configmenu, Fuzzysearch(Menu), Openmenu.
 }

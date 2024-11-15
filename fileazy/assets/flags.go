f887f0d3465ba9f877f12fcb2b390983ef3740f9
TRC Loop <aro.yt.mail@gmail.com>
2024-11-16 00:30:11 +0100

    Flag to directly edit the Config without having to go into the menu

--- fileazy/assets/flags.go
+++ fileazy/assets/flags.go
@@ -1,8 +1,9 @@
 package assets
 
 const (
-	FileHelp          = "Only show Opening Menu and Open File with Program"
-	NoOpenHelp        = "Only show Fuzzy Search Menu without Opening the File"
-	OpenConfigHelp    = "Open Config(Settings) Menu"
-	SearchForFileHelp = "Search for a File, Will show a list of Files/Paths."
+	FileHelp               = "Only show Opening Menu and Open File with Program"
+	NoOpenHelp             = "Only show Fuzzy Search Menu without Opening the File"
+	OpenConfigHelp         = "Open Config(Settings) Menu"
+	SearchForFileHelp      = "Search for a File, Will show a list of Files/Paths."
+	EditConfigDirectlyHelp = "Edit Config Directly. Use 'Key:Value' Format, to change a Value in the Config."
 )
--- fileazy/main.go
+++ fileazy/main.go
@@ -12,12 +12,13 @@ func main() {
 	noOpen := flag.Bool("no", false, assets.NoOpenHelp)
 	openConfig := flag.Bool("c", false, assets.OpenConfigHelp)
 	searchForFile := flag.String("s", "", assets.SearchForFileHelp)
+	editConfig := flag.Bool("ec", false, assets.EditConfigDirectlyHelp)
 
 	// Parse Flags
 	flag.Parse()
 
 	// Print Flags (for Debugging)
-	fmt.Printf("Flags: file: %s, noOpen: %t, openConfig: %t searchForFile: %s\n", *file, *noOpen, *openConfig, *searchForFile)
+	fmt.Printf("Flags: file: %s, noOpen: %t, openConfig: %t searchForFile: %s ec: %t\n", *file, *noOpen, *openConfig, *searchForFile, *editConfig)
 
 	// TODO: Implement Configmenu, Fuzzysearch(Menu), Openmenu.
 }

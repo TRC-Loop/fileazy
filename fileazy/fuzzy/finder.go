5faaf8e2e25c5d140079af1753fd87c12305eb41
Lasse <lamaxko@icloud.com>
2024-11-18 21:44:57 +0100

    added fuzzy finder

---
+++ fileazy/fuzzy/finder.go
@@ -0,0 +1,16 @@
+package fuzzy
+
+import (
+    "os"
+    "io/fs"
+)
+
+func fuzzyFinder(path string) var {
+
+    var file_paths []string
+
+    if not ValidPath(path) {
+    return false
+    }
+
+}
